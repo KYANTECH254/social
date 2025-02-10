d7b643cd1c214596ac740cd7df471e63585079ea
KYANTECH254 <kyantech254@gmail.com>
2025-02-10 10:47:46 -0500

    chat update

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,33 +1,33 @@
 // import StatusViewer from "@/components/pages/StatusViewer";
 
-// interface Status {
-//     id: string;
-//     type: 'image' | 'video' | 'text';
-//     content: string;
-//   }
+interface Status {
+    id: string;
+    type: 'image' | 'video' | 'text';
+    content: string;
+  }
 
-// const sampleStatuses = [
-//   {
-//     id: '1',
-//     type: 'image',
-//     content: 'assets/images/pic.jpeg',
-//   },
-//   {
-//     id: '2',
-//     type: 'video',
-//     content: 'assets/images/video.mp4',
-//   },
-//   {
-//     id: '3',
-//     type: 'text',
-//     content: 'Hello World!',
-//   },
-// ];
+const sampleStatuses = [
+  {
+    id: '1',
+    type: 'image',
+    content: 'assets/images/pic.jpeg',
+  },
+  {
+    id: '2',
+    type: 'video',
+    content: 'assets/images/video.mp4',
+  },
+  {
+    id: '3',
+    type: 'text',
+    content: 'Hello World!',
+  },
+];
 
-// export default function StatusPage() {
-//   return (
-//     <div className="h-screen">
-//       <StatusViewer statuses={sampleStatuses} />
-//     </div>
-//   );
-// }
+export default function StatusPage() {
+  return (
+    <div className="h-screen">
+      {/* <StatusViewer statuses={sampleStatuses} /> */}
+    </div>
+  );
+}
