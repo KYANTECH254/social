879c80ca6cff12373b5d7aa4b63063b8ecbf5af5
KYANTECH254 <kyantech254@gmail.com>
2025-02-27 09:28:00 -0500

    initial commit

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,28 +1,28 @@
-import StatusViewer from "@/components/pages/StatusViewer";
-import { StatusPost } from "@/types/types";
+// import StatusViewer from "@/components/pages/StatusViewer";
+// import { StatusPost } from "@/types/types";
 
 export default function StatusPage() {
-  const sampleStatuses: StatusPost[] = [
-    {
-      id: "1",
-      type: "image",
-      content: "assets/images/pic.jpeg",
-    },
-    {
-      id: '2',
-      type: 'video',
-      content: 'assets/images/video.mp4',
-    },
-    {
-      id: '3',
-      type: 'text',
-      content: 'Hello World!',
-    }
-  ];
+  // const sampleStatuses: StatusPost[] = [
+  //   {
+  //     id: "1",
+  //     type: "image",
+  //     content: "assets/images/pic.jpeg",
+  //   },
+  //   {
+  //     id: '2',
+  //     type: 'video',
+  //     content: 'assets/images/video.mp4',
+  //   },
+  //   {
+  //     id: '3',
+  //     type: 'text',
+  //     content: 'Hello World!',
+  //   }
+  // ];
 
   return (
     <div className="h-screen">
-      <StatusViewer statuses={sampleStatuses} />
+      {/* <StatusViewer statuses={sampleStatuses} /> */}
     </div>
   );
 }
